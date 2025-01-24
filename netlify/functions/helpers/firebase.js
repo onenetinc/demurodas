c5d153a87f6eb1088fcd34cf0a4a4bcf441af37c
Jeremy <jeremy@onenetinc.com>
2025-01-24 13:55:51 -0700

    update gcp service account env

--- netlify/functions/helpers/firebase.js
+++ netlify/functions/helpers/firebase.js
@@ -1,8 +1,13 @@
 // netlify/functions/helpers/firebase.js
 const admin = require('firebase-admin');
+// dotenv config
+require('dotenv').config();
 
 const serviceAccountBuffer = Buffer.from(process.env.SERVICE_ACCOUNT, 'base64');
 const serviceAccount = JSON.parse(serviceAccountBuffer.toString('utf8'));
+
+// console.log("serviceAccountBuffer: ", serviceAccountBuffer);
+// console.log("serviceAccount: ", serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
