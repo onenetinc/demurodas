26575b492d7123c45d179038ec066ba1357b1299
Charles <charles@onenetinc.com>
2025-02-07 15:11:28 -0600

    switch to Browserless

--- netlify/functions/helpers/createProductPdfs.js
+++ netlify/functions/helpers/createProductPdfs.js
@@ -53,22 +53,10 @@ const createProductPdfs = async (slug) => {
       console.log("✅ Chromium is correctly installed and executable.");
 
       // Launch Playwright browser
-      browser = await playwrightChromium.launch({
-        executablePath,
-        headless: true,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-accelerated-2d-canvas',
-          '--disable-gpu',
-          '--single-process',
-          '--disable-features=site-per-process',
-          '--disable-translate',
-          '--disable-sync',
-          '--no-zygote',
-        ],
-      });
+      const BROWSERLESS_API_KEY = "RjRbvDDTnIm2vN2d0126163c4ba7de95be0a42f050";
+
+      browser = await playwrightChromium.connectOverCDP(`wss://chrome.browserless.io?token=${BROWSERLESS_API_KEY}`);
+
 
       if (!browser) {
         console.error("❌ ERROR: Browser instance is NULL! Chromium may have crashed.");
