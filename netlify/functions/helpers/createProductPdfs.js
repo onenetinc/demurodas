5dbeecb7ebd582a6c02a36449ea5fe4d78576524
Charles <charles@onenetinc.com>
2025-02-07 14:01:15 -0600

    tweaks

--- netlify/functions/helpers/createProductPdfs.js
+++ netlify/functions/helpers/createProductPdfs.js
@@ -60,7 +60,6 @@ const createProductPdfs = async (slug) => {
           '--disable-gpu',
           '--no-sandbox',
           '--disable-setuid-sandbox',
-          '--single-process',
           '--disable-dev-shm-usage',
           '--disable-accelerated-2d-canvas',
           '--disable-web-security',
@@ -69,19 +68,23 @@ const createProductPdfs = async (slug) => {
           '--hide-scrollbars',
           '--disable-background-networking',
           '--disable-software-rasterizer',
+          '--no-zygote',
+          '--single-process',
         ],
       });
 
       const page = await browser.newPage();
       await page.setViewportSize({ width: 1420, height: 2000 });
-
+      // **Ensure Chromium fully loads before navigation**
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      
       // **Retry page.goto() in case of failures**
       let maxRetries = 3;
       let attempt = 0;
       while (attempt < maxRetries) {
         try {
           await page.goto(`https://demurodas.webflow.io/products/${slug}?mode=server`, {
-            waitUntil: 'domcontentloaded', // ✅ Faster & more reliable in Netlify
+            waitUntil: 'load', // ✅ More stable than 'networkidle' in Netlify
             timeout: 20000,
           });
           console.log('✅ Page loaded successfully.');
