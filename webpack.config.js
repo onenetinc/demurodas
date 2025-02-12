8cb1c9463d903bd9fbc7e9e8726595103af26e1f
Charles <charles@onenetinc.com>
2025-02-12 13:32:13 -0600

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
         '@sparticuz/chromium': 'commonjs @sparticuz/chromium',  // ✅ Ensure correct Chromium externalization
         yargs: 'commonjs yargs',
         'yargs-parser': 'commonjs yargs-parser',
+        "puppeteer-core": "commonjs puppeteer-core",
         "puppeteer-extra": "commonjs puppeteer-extra",
         "puppeteer-extra-plugin-stealth": "commonjs puppeteer-extra-plugin-stealth",
         "puppeteer": "commonjs puppeteer",
