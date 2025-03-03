f28fd5c647b841a2b263e9c43f6155e2e510ca6b
Jeremy <jeremy@onenetinc.com>
2025-03-03 16:03:01 -0700

    Revert "test2"
    This reverts commit d92c251d9ab22898948988e6abde28ed937e68c3.

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,9 @@ module.exports = {
         yargs: 'commonjs yargs',
         'yargs-parser': 'commonjs yargs-parser',
         "puppeteer-core": "commonjs puppeteer-core",
+        "puppeteer-extra": "commonjs puppeteer-extra",
+        "puppeteer-extra-plugin-stealth": "commonjs puppeteer-extra-plugin-stealth",
+        "puppeteer": "commonjs puppeteer",
     },
     module: {
         rules: [
