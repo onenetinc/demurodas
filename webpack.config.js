53555842ee87f574255e5664f6826f594e7ae197
Jeremy <jeremy@onenetinc.com>
2025-03-03 14:05:01 -0700

    add node fetch to externals

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,8 @@ module.exports = {
         yargs: 'commonjs yargs',
         'yargs-parser': 'commonjs yargs-parser',
         "puppeteer-core": "commonjs puppeteer-core",
-        'puppeteer': 'commonjs puppeteer'
+        'puppeteer': 'commonjs puppeteer',
+        'node-fetch': 'commonjs node-fetch'
     },
     module: {
         rules: [
