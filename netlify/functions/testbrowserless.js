0304ffe86f147bfe183e540f432dbca2eedb766c
Jeremy <jeremy@onenetinc.com>
2025-03-03 16:03:28 -0700

    Revert "change url"
    This reverts commit 589a2d2054a1d891fb711c82e1976f98c31dd5a0.

--- netlify/functions/testbrowserless.js
+++ netlify/functions/testbrowserless.js
@@ -23,7 +23,7 @@ exports.handler = async () => {
 
     // **💡 Test URL**
     const productSlug = "athena-side-table"; // Change slug dynamically later
-    const testURL = `https://google.com/`;
+    const testURL = `https://demurodas.webflow.io/products/${productSlug}?mode=server`;
 
     console.log(`🌍 Navigating to ${testURL}...`);
 
